e4f9cdc9445652e1a1e8e561ace19a47e4a07fd2
jaironsousa1 <jaironsousa@gmail.com>
2019-05-28 21:20:29 -0300

    Ajuste Layout nav, footer e conteudo

---
+++ src/main/resources/insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO estabelecimento(nome, responsavel, status, conta) values ('Açai do Bom', 'Jairo', 'RECEBIDO', 'FREE');
--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
     selector: 'jhi-footer',
-    templateUrl: './footer.component.html'
+    templateUrl: './footer.component.html',
+    styleUrls: ['footer.scss']
 })
-export class FooterComponent {}
+export class FooterComponent {
+    facebook = faFacebook;
+    twitter = faTwitter;
+    instagram = faInstagram;
+}
